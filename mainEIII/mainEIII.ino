f689a2eb70ce4d63ce6958c9aa7babd01e7dce8e
unknown <klgills@ENGT284-60201.students.eden.ualr.edu>
2015-04-12 19:43:43 -0500

    Rework sensor pinging

--- mainEIII/mainEIII.ino
+++ mainEIII/mainEIII.ino
@@ -14,9 +14,14 @@
 #define YM 54   // can be a digital pin, this is A0
 #define XP 57   // can be a digital pin, this is A3
 
+#define srr 0
+#define slf 1
+#define sff 2
+#define slr 3
+#define srf 4
 
 //function declarations
-float ping_median(NewPing* sensor);
+float ping_median(NewPing* sensor, float avg, int i, int j);
 void stablize();
 void rotate();
 void exec_command();
@@ -28,6 +33,9 @@ int bound(int num, int zero, int dead, int top);
 
 //variable declarations
 unsigned long time = millis();
+unsigned long old_time = time;
+unsigned long elapsed = 0;
+
 byte readbyte;
 byte writebyte;
 boolean command_stat;
@@ -35,10 +43,20 @@ byte dir;
 byte move_blocks;
 const int unit = 12;
 
-unsigned int cruise = 2200;
+unsigned int cruise = 2600;
 unsigned int dead_zone = 0;
-unsigned int max_speed = 800;
+unsigned int max_speed = 400;
 unsigned int zero = 2048;
+
+float r_acc = 0.0;
+float r_vel = 0.0;
+float r_pos = 0.0;
+
+float l_acc = 0.0;
+float l_vel = 0.0;
+float l_pos = 0.0;
+
+float a_max = 50.0;
 
 word target_right = 0;  //only pass this ints, i tried doing math in this and the remainder error screwed something up
 word target_left = 0;
@@ -49,9 +67,20 @@ NewPing* sonlr;
 NewPing* sonrr;
 NewPing* sonff;
 
+NewPing* sensors[5];
+
 HughesyShiftBrite* sb;
 
 void setup(){
+  
+    Serial1.begin(115200);
+    Serial2.begin(115200);
+    Serial.begin(115200);
+    Serial.flush();
+    Serial1.flush();
+    Serial2.flush();
+    set_speed(zero, zero);
+  
 //    pinMode(37, OUTPUT);
 //    pinMode(39, OUTPUT);
 //    digitalWrite(37, HIGH);
@@ -61,27 +90,22 @@ void setup(){
     sb = new HughesyShiftBrite(52, 50, 48, 46);
     sb->sendColour(0, 0, 1023);
 
-
     sonlf = new NewPing(23, 22, 50);
     sonrf = new NewPing(25, 24, 50);
     sonlr = new NewPing(27, 26, 50);
     sonrr = new NewPing(31, 30, 50);
-    sonff = new NewPing(29, 28, 50);
-
-    Serial1.begin(115200);
-    Serial2.begin(115200);
-    Serial.begin(115200);
-    Serial.flush();
-    Serial1.flush();
-    Serial2.flush();
-
+    sonff = new NewPing(29, 28, 215);
+    
+    sensors[slf] = sonlf;
+    sensors[srr] = sonrr;
+    sensors[sff] = sonff;
+    sensors[slr] = sonlr;
+    sensors[srf] = sonrf;
+    
     Tft.init();  //init TFT library
     Tft.drawString("UALR",0,25,4,WHITE);
     Tft.drawString("Robotics",25,80,3,WHITE);
     Tft.drawString("^_^",30 ,200,8,WHITE);
-
-
-    set_speed(zero, zero);
 }
 
 void loop() {
@@ -94,77 +118,120 @@ void loop() {
     }
 }
 
-float ping_median(NewPing* sensor){
-    float avg = 12;
-    int pini;
-    for(int i = 0; i < 3; i++){
+float ping_median(NewPing* sensor, float avg, int i, int n)
+{
+  
+    float pini = sensor->ping();
+    if(pini == 0){
+        Serial.print("F: " );
+        Serial.print(n);
+        Serial.println();
         pini = sensor->ping();
-        delay(23);
         if(pini == 0){
             pini = sensor->ping();
-            if(pini == 0){
-                pini = sensor->ping();
-            }
         }
-        avg = ((avg*i) + pini)/(i+1);
     }
+    avg = ((avg*i) + pini)/(i+1);
     return avg;
 }
 
 void stablize(){
-    float tolf = ping_median(sonlf);
-    float tolr = ping_median(sonlr);
-    float torf = ping_median(sonrf);
-    float torr = ping_median(sonrr);
-    float toff = ping_median(sonff);
+    unsigned long start = millis();
+    
+    float avg[5] = {0.0, 0.0, 0.0, 0.0, 0.0};
+    
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 5; j++)
+        {
+            avg[j] = ping_median(sensors[j], avg[j], i, j);
+        }
+    }
 
-    float rfc = torf / US_ROUNDTRIP_CM;
-    float rrc = torr / US_ROUNDTRIP_CM;
-    float lfc = tolf / US_ROUNDTRIP_CM;
-    float lrc = tolr / US_ROUNDTRIP_CM + 0.5;
+    float rfc = avg[srf] / US_ROUNDTRIP_CM;
+    float rrc = avg[srr] / US_ROUNDTRIP_CM;
+    float lfc = avg[slf] / US_ROUNDTRIP_CM;
+    float lrc = avg[slr] / US_ROUNDTRIP_CM + 0.5;
 
     float rangle = atan((rrc-rfc)/8.0)*180/3.14159;
     float langle = -1*atan((lrc-lfc)/8.25)*180/3.14159;
     float avg_angle = (rangle+langle)/2.0;
-
-    if(abs(avg_angle)<2.0)
-    {
-        Serial.println("Cruising");
-        target_left = cruise;
-        target_right = cruise;
-    }
-    if(abs(avg_angle)>15.0)
-    {
-        if(avg_angle > 0.0) //need to rotate left
-        {
-            Serial.println("Correcting to go left");
-            target_left = cruise-max_speed*0.05;
-            target_right = cruise+max_speed*0.05;
-        }
-        else if(avg_angle < 0.0) //need to rotate right
-        {
-            Serial.println("Correcting to go right");
-            target_left = cruise+max_speed*0.05;
-            target_right = cruise-max_speed*0.05;
-        }
-    }
-    else {
-        Serial.println("Within angle, doing normal corrections");
-        target_left = cruise-max_speed*(avg_angle/180.0);
-        target_right = cruise+max_speed*(avg_angle/180.0);
-    }
-
     
-    Serial.print("right: \t");    
-    Serial.print(rangle);
-    Serial.print("\t left: \t");
-    Serial.print(langle);
-    Serial.print("\t average: \t");
-    Serial.println((rangle+langle)/2.0);
-    Serial.print("left speed: \t");
-    Serial.print(target_left);
-    Serial.print(" \tright speed: \t");
-    Serial.println(target_right);
+    float correction_speed = 0.0;
+    
+    if(abs(avg_angle)>3.0)
+    {
+      correction_speed = (avg_angle/30.0)*max_speed*0.5;
+      
+      l_vel = (cruise-zero)-correction_speed;
+      r_vel = (cruise-zero)+correction_speed;
+      
+//    if(abs(avg_angle)>15.0)
+//    {
+//        if(avg_angle > 0.0) //need to rotate left
+//        {
+//            Serial.println("Correcting to go left");
+//            l_acc -= 5.0;
+//            r_acc += 5.0;
+//            //target_left = cruise-max_speed*0.05;
+//            //target_right = cruise+max_speed*0.05;
+//        }
+//        else if(avg_angle < 0.0) //need to rotate right
+//        {
+//            Serial.println("Correcting to go right");
+//            l_acc += 5.0;
+//            r_acc -= 5.0;
+//            //target_left = cruise+max_speed*0.05;
+//            //target_right = cruise-max_speed*0.05;
+//        }
+//    }
+
+//        if(avg_angle > 0.0)
+//        {
+//            Serial.println("Turning left");
+//            l_acc -= avg_angle/15.0;
+//            r_acc += avg_angle/15.0;
+//        }  
+//        else {
+//            Serial.println("Turning right");
+//            l_acc -= avg_angle/15.0;
+//            r_acc += avg_angle/15.0;
+//        }
+        //target_left = cruise-max_speed*(avg_angle/180.0);
+        //target_right = cruise+max_speed*(avg_angle/180.0);
+    }
+    else
+    {
+      l_acc = 0.0;
+      r_acc = 0.0;      
+      l_vel = max_speed / 3.0;
+      r_vel = max_speed / 3.0;      
+    }
+
+//    if(r_vel > cruise-zero) r_acc -= 5.0;
+//    if(l_vel > cruise-zero) l_acc -= 5.0;
+//    if(r_vel < cruise-zero) r_acc += 5.0;
+//    if(l_vel < cruise-zero) l_acc += 5.0;
+//
+//    if(l_acc > a_max) l_acc = a_max;
+//    if(l_acc < -a_max) l_acc = -a_max;
+//    if(r_acc > a_max) r_acc = a_max;
+//    if(r_acc < -a_max) r_acc = -a_max;
+    
+//    Serial.print("right: \t");    
+//    Serial.print(rangle);
+//    Serial.print("\t left: \t");
+//    Serial.print(langle);
+//    Serial.print("\t average: \t");
+//    Serial.println((rangle+langle)/2.0);
+//    Serial.print("left speed: \t");
+//    Serial.print(l_vel);
+//    Serial.print(" \tleft acc: \t");
+//    Serial.println(l_acc);
+//    Serial.print("right speed: \t");
+//    Serial.print(r_vel);
+//    Serial.print(" \tright acc: \t");
+//    Serial.println(r_acc);
+//    Serial.println();
 
 //    if( abs(tolf)>0.1 && abs(tolr)>0.1 && abs(torf)>0.1 && abs(torr)>0.1)
 //    {
@@ -204,23 +271,24 @@ void rotate(){
 void exec_command(){
     set_speed(cruise, cruise);
     while(true){
+        time = millis();
         stablize();
         set_speed(target_left, target_right);
     }
 }
 
 void announce_sensors(){
-    Serial.print("lf ");
-    Serial.print(ping_median(sonlf));
-    Serial.print(" lr ");
-    Serial.print(ping_median(sonlr));
-    Serial.print(" rf ");
-    Serial.print(ping_median(sonrf));
-    Serial.print(" rr ");
-    Serial.print(ping_median(sonrr));
-    Serial.print(" ff ");
-    Serial.print(ping_median(sonff));
-    Serial.print("\n");
+//    Serial.print("lf ");
+//    Serial.print(ping_median(sonlf));
+//    Serial.print(" lr ");
+//    Serial.print(ping_median(sonlr));
+//    Serial.print(" rf ");
+//    Serial.print(ping_median(sonrf));
+//    Serial.print(" rr ");
+//    Serial.print(ping_median(sonrr));
+//    Serial.print(" ff ");
+//    Serial.print(ping_median(sonff));
+//    Serial.print("\n");
 }
 
 int get_int(int chars){
@@ -229,8 +297,12 @@ int get_int(int chars){
 }
 
 void set_speed(int left, int right) {
-    right = bound(right, zero, dead_zone, max_speed);
-    left = bound(left, zero, dead_zone, max_speed);
+    old_time = millis();
+    elapsed = old_time - time;
+    r_vel = r_vel + r_acc*elapsed/1000.0;
+    l_vel = l_vel + l_acc*elapsed/1000.0;
+    right = bound(r_vel+zero, zero, dead_zone, max_speed);
+    left = bound(l_vel+zero, zero, dead_zone, max_speed);
 //    Serial.print(left);
 //    Serial.print(" ");
 //    Serial.print(right);
